2698746238c80f4bcb90626034a2ab1b572529e4
jinfeng <qianjinfeng@outlook.com>
2025-02-17 21:43:01 +0800

    parse dicom frame

--- scu/app.py
+++ scu/app.py
@@ -1,7 +1,9 @@
 import os
 from pynetdicom import AE, evt, AllStoragePresentationContexts, ALL_TRANSFER_SYNTAXES
 from pynetdicom.sop_class import Verification
+import pydicom
 from pydicom.tag import Tag
+from pydicom.encaps import generate_pixel_data_frame
 from pika import ConnectionParameters, BlockingConnection, PlainCredentials
 import json
 import requests
@@ -68,20 +70,38 @@ class MyStorage(object):
     def bulk_data_handler(self, data_element):
         if data_element.VR in ['OB', 'OD', 'OF', 'OL', 'OV', 'OW']:
             file_name = f'{data_element.tag:08x}'  # 将tag转换为十六进制字符串
-            # 准备POST请求的数据
-            files = {'file': (file_name, data_element.value)}
-            try:
-                # 发送POST请求
-                response = requests.post(self.api_url, files=files)
-                response.raise_for_status()  # 抛出HTTP错误
-                if response.status_code == 200:
-                    result = response.json()
-                    logging.info(f'BulkData added to IPFS: {result["Hash"]}')
-                    return result['Hash']
-            except Exception as e:
-                logging.error(f"Failed to store DICOM image to IPFS: {e}")
-                # 可能在此处实现重试机制或其他恢复策略
-                return data_element.value
+            if data_element.tag == pydicom.tag.Tag(0x7fe0, 0x0010):
+                frames = generate_pixel_data_frame(data_element.value);
+                # 逐项处理每个 bytes 对象
+                for chunk in frames:
+                    # 处理每个 chunk（bytes 对象）
+                    print(f"frame size; {len(chunk)}")
+                    # 准备POST请求的数据
+                    files = {'file': (file_name, chunk)}
+                    
+                    try:
+                        # 发送POST请求
+                        response = requests.post(self.api_url, files=files)
+                        response.raise_for_status()  # 抛出HTTP错误
+                    except Exception as e:
+                        print(f"Failed to store DICOM pixel to IPFS: {e}")
+            else:
+                files = {'file': (file_name, data_element.value)}
+                try:
+                    # 发送POST请求
+                    response = requests.post(self.api_url, files=files)
+                    response.raise_for_status()  # 抛出HTTP错误
+                except Exception as e:
+                    print(f"Failed to store DICOM other than pixel to IPFS: {e}")
+
+            # 检查响应状态码
+            if response.status_code == 200:
+                result = response.json()
+                print(f'BulkData added to IPFS: {result["Hash"]}')
+                return result['Hash']
+            else:
+                print(f'Error adding BulkData to IPFS: {response.status_code} - {response.text}')
+
         else:
             logging.warning(f"Unsupported VR: {data_element.VR}")
             return data_element.value
@@ -92,6 +112,21 @@ class MyStorage(object):
             ds = event.dataset
             ds.file_meta = event.file_meta
 
+            # 检查并设置PatientSex默认值
+            if 'PatientSex' not in ds or not ds.PatientSex:
+                print("Setting default value for PatientSex.")
+                ds.PatientSex = 'O'  # 设置默认值为 'O' (Other)
+
+            # 检查并设置StudyID默认值
+            if 'StudyID' not in ds or not ds.StudyID:
+                print("Setting default value for StudyID.")
+                ds.StudyID = 'NOID'  # 设置默认值
+            
+            # 检查并设置PatientID默认值
+            if 'PatientID' not in ds or not ds.PatientID:
+                print("Setting default value for PatientID.")
+                ds.StudyID = 'NOID'  # 设置默认值
+                
             self.currentSOPinstanceUID = ds['SOPInstanceUID'].value
             self.instanceNR = ds['InstanceNumber'].value
 
