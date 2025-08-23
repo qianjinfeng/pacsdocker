81d14e6cb4a70d3e04bc3e0f72a387cdf0dc01e3
qianjinfeng <qianjinfeng@outlook.com>
2025-08-23 17:43:21 +0800

    port redefine

--- converter/config/env.js
+++ converter/config/env.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const config = {
   rabbitmq: {
-    url: process.env.RABBITMQ_URL || 'amqp://localhost:5673',
+    url: process.env.RABBITMQ_URL || 'amqp://localhost:5672',
     queue: process.env.RABBITMQ_QUEUE || 'dicom_queue'
   },
   elasticsearch: {
